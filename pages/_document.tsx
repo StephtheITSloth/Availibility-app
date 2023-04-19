ef2d361eb2c2f7248708b322c6ad3868e3872d41
stephtheitsloth <stephan.karim.sk@gmail.com>
2023-04-19 08:34:43 -0400

    Refactor: Clean up code to implement DRY

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import NavBar from '../componenets/NavBar'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+      <main className="bg-gray-100 min-h-screen w-screen text-black">
+    <main className="max-w-screen-2xl m-auto bg-white">
+      <NavBar />
         <Main />
         <NextScript />
+        </main>
+        </main>
       </body>
     </Html>
   )
